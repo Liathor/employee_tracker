bf83bafc5135822f27ed82062197b660abc7116b
Liathor <dyermisha@gmail.com>
2025-02-22 12:06:32 -0500

    Added schema.sql and downloaded inquirer package

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+-- Create database for employees and navigate to it--
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+\c employees_db;
+
+
+-- Create tables for employees database--
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
